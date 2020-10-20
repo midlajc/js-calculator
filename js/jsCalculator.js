09276df49ccc1a5150dd4306134d36a90da5f1c1
Midlaj C <midlajc@gmail.com>
2020-10-20 15:51:41 +0530

    added oerator auto disable

--- js/jsCalculator.js
+++ js/jsCalculator.js
@@ -26,124 +26,110 @@ document.getElementById("backSpace").addEventListener("click",backSpace)
 //button click action
 
 function oneClicked(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"1"
     document.getElementById("display").value=newValue
-
+    enableOperator()
 }
 
 function twoClicked(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"2"
     document.getElementById("display").value=newValue
-
+    enableOperator()
 }
 
 function threeClicked(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"3"
     document.getElementById("display").value=newValue
-
+    enableOperator()
 }
 
 function fourClicked(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"4"
     document.getElementById("display").value=newValue
-
+    enableOperator()
 }
 
 function fiveClicked(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"5"
     document.getElementById("display").value=newValue
-
+    enableOperator()
 }
 
 function sixClicked(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"6"
     document.getElementById("display").value=newValue
-
+    enableOperator()
 }
 
 function sevenClicked(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"7"
     document.getElementById("display").value=newValue
-
+    enableOperator()
 }
 
 function eightClicked(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"8"
     document.getElementById("display").value=newValue
-
+    enableOperator()
 }
 
 function nineClicked(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"9"
     document.getElementById("display").value=newValue
-
+    enableOperator()
 }
 
 function zeroClicked(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"0"
     document.getElementById("display").value=newValue
-
+    enableOperator()
 }
 
 function dotClicked(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"."
     document.getElementById("display").value=newValue
     document.getElementById("dot").disabled=true
-
+    enableOperator()
 }
 
 //operator click action
 
 function add(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"+"
     document.getElementById("display").value=newValue
     document.getElementById("dot").disabled=false
+    disableOperator()
 }
 
 function sub(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"-"
     document.getElementById("display").value=newValue
     document.getElementById("dot").disabled=false
- 
+    disableOperator()
 }
 
 function div(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"/"
     document.getElementById("display").value=newValue
     document.getElementById("dot").disabled=false
- 
+    disableOperator()
  }
 
  function mul(){
-
     let oldVlaue=document.getElementById("display").value
     let newValue=oldVlaue+"*"
     document.getElementById("display").value=newValue
@@ -153,7 +139,6 @@ function div(){
 //clear button action
 
 function backSpace(){
-
     let currentValue=document.getElementById("display").value
     strrev=reverse(currentValue)
     strrev=strrev.replace(strrev.charAt(0),"")
@@ -163,24 +148,35 @@ function backSpace(){
 }
 
 function allClear(){
-
     document.getElementById("display").value = ""
     document.getElementById("dot").disabled = false
-
 }
 
 //calculate
 
 function calculate(){
-
     let displayVlaue=document.getElementById("display").value
     let result=eval(displayVlaue)
     document.getElementById("display").value = result
     document.getElementById("dot").disabled = false
-
 }
 
 
-function reverse(s){
+let reverse=(s)=>{
     return s.split("").reverse().join("");
+}
+
+let disableOperator=()=>{
+    document.getElementById("mul").disabled = true
+    document.getElementById("sub").disabled = true
+    document.getElementById("add").disabled = true
+    document.getElementById("div").disabled = true
+}
+
+let enableOperator=()=>{
+    document.getElementById("mul").disabled = false
+    document.getElementById("sub").disabled = false
+    document.getElementById("add").disabled = false
+    document.getElementById("div").disabled = false
+
 }
